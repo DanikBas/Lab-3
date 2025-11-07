95fc4b67be26f8a05cc19a2f7c63b73053cef4ad
lare3456 <lare3456@colorado.edu>
2025-11-07 15:03:52 -0700

    Add files via upload

--- call_to_NACAgen.m
+++ call_to_NACAgen.m
@@ -1,8 +1,6 @@
 clear; clc; close all;
 
-%% ~~~~~~~~~~~~ Task 1 ~~~~~~~~~~~~~~
 % want NACA 0018 and 2418
 
 % inputs: NACA code, cord length, number of panels
-[xU1,yU1, xL1,yL1, xU2,yU2, xL2,yL2] = NACA_gen([0 0 18] , 10, 10);
-                                           %code   %cord_line  %#ofPanels
+[xU2, yU2, xL2, yL2] = NACA_gen([0 0 18] , 10, 10);
