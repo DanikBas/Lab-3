a3e00096d70a1e8cb9905ca35e8821597d581e6a
BuffBotBrawlers <BuffBotBrawlers@colorado.edu>
2025-11-07 15:47:40 -0700

    task 2.1

--- main.m
+++ main.m
@@ -1,16 +1,33 @@
 clear; clc; close all;
 
 %% ~~~~~~~~~~ Task 2 ~~~~~~~~~~~
-% % ----Task 2.1-----
-%     % NACA 0012
-%     % Varriables           
-%     x_b = 0 ;
-%     y_b = 0 ; 
-%     alpha = 5;
-% 
-%     c_l = Vortex_Panel(x_b, y_b, alpha);
-% 
-%     % Plot c_l vs num_panels
+% ----Task 2.1-----
+    % NACA 0012
+    % Varriables           
+    alpha = 5;
+
+    numOfPanels = linspace(1,100,100);
+    c_l = linspace(1,100,100);
+    for i = 1:length(numOfPanels)
+         % Get boundary Conditions
+        [xU2, yU2, xL2, yL2] = NACA_gen([0 0 12] , 10, i);
+
+            % x_b and y_b are the boundary coordinates starting at the trailing
+            % edge and going clockwise 
+            % the leading edge is zero zero 
+
+            x_b = zeros(1, length(xL2) + length(xU2));
+            x_b(1:length(xL2)) = xL2';
+            x_b(length(xL2)+1 : length(xL2) + length(xU2)) = xU2;
+
+            y_b = zeros(1,length(yL2) + length(yU2));
+            y_b(1:length(yL2)) = yL2';
+            y_b(length(yL2)+1 : length(yL2) + length(yU2)) = yU2;
+
+
+        c_l(i) = Vortex_Panel(x_b, y_b, alpha);
+    end
+    % Plot c_l vs num_panels
 
 % ----Task 2.2------
     NACA = ["0006" "0012" "0018"];
