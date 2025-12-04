9feb1c01b9295e7b11edda6d9e3544b3ea02b257
lare3456 <lare3456@colorado.edu>
2025-12-03 17:38:00 -0700

    Add files via upload

--- Part 3/taks3ofPart3.m
+++ Part 3/taks3ofPart3.m
@@ -10,9 +10,9 @@ c_r = 5 + 4/12;     % root chord[ft]
 c_t = 3 + 7/12;     % tip chord[ft]
 
 % 2D airfoil data (from Part 2)
-a0_r    =  0.1048;  % lift slope at root (per rad)
-a0_t    =  0.1051;  % lift slope at tip (per rad)
-aero_r  = deg2rad(-2);        % root alpha_L=0 [rad]
+a0_r    = 0.1048; %*180/pi;  % lift slope at root (per rad)
+a0_t    = 0.1051; %*180/pi;  % lift slope at tip (per rad)
+aero_r  = -2; %deg2rad(-2);        % root alpha_L=0 [rad]
 aero_t  = 0;  % tip alpha_L=0 [rad]
 S = b * (c_r + c_t) / 2;  % planform area [ft^2]
 
@@ -28,11 +28,11 @@ C_Di = zeros(size(alpha_tip_deg)); % FIXED
 C_L_vec       = zeros(size(alpha_tip_deg)); % GOOD
 
 for k = 1:length(alpha_tip_deg)
-    geo_t = deg2rad(alpha_tip_deg(k));           % tip AoA
-    geo_r = deg2rad(alpha_tip_deg(k) + 2.0);     % root AoA with +2° twist
+     geo_t = alpha_tip_deg(k); %deg2rad(alpha_tip_deg(k));           % tip AoA
+     geo_r = alpha_tip_deg(k) + 2.0; %deg2rad(alpha_tip_deg(k) + 2.0);     % root AoA with +2° twist
 
     % Call PLLT
-    [e, C_L_vec(k), C_Di(k)] = PLLT_Submission(b, a0_t, a0_r, c_t, c_r,aero_t, aero_r, geo_t, geo_r, N); % inputs need to be in rad
+    [e, C_L_vec(k), C_Di(k)] = PLLT_submission(b, a0_t, a0_r, c_t, c_r,aero_t, aero_r, geo_t, geo_r, N); % FIXED
 end
 
 
@@ -43,9 +43,9 @@ alpha = linspace(-10,6,9);
 cl_exp_tip = linspace(-0.8,0.8,9);
 cd_exp_tip = [0.018,0.0135,0.0115,0.0102,0.0098,0.0103,0.0115,0.014,0.018];
 
-%Root NACA 2412
-cl_exp_root = linspace(-0.8,1,9); 
-cd_exp_root = [0.022,0.017,0.013,0.011,0.010,0.0099,0.010,0.011,0.0135];
+% %Root NACA 2412
+% cl_exp_root = linspace(-0.8,1,9); 
+% cd_exp_root = [0.022,0.017,0.013,0.011,0.010,0.0099,0.010,0.011,0.0135];
 
 %check experimental data by plotting
 %cl vs cd tip
@@ -54,17 +54,17 @@ cl_fit = linspace( min(cl_exp_tip),max(cl_exp_tip),200);
 cd_fit = polyval(p_tip,cl_fit);
 
 
-%cl vs cd root
-p_root = polyfit(cl_exp_root,cd_exp_root,2);
-x_fit_root = linspace( min(cl_exp_root),max(cl_exp_root),200);
-y_fit_root = polyval(p_root,x_fit_root);
+% %cl vs cd root
+% p_root = polyfit(cl_exp_root,cd_exp_root,2);
+% x_fit_root = linspace( min(cl_exp_root),max(cl_exp_root),200);
+% y_fit_root = polyval(p_root,x_fit_root);
 
 
 %cl vs alpha tip
 p_tip2 = polyfit(alpha,cl_exp_tip,1);
 
-%cl vs alpha tip
-p_root2 = polyfit(alpha,cl_exp_root,1);
+% %cl vs alpha tip
+% p_root2 = polyfit(alpha,cl_exp_root,1);
 
 alpha_model = linspace(-20,20,200);
 cl_model = polyval(p_tip2, alpha_tip_deg);  %FIXED      % estimated cl
@@ -78,31 +78,23 @@ cd_model = polyval(p_tip, cl_model);            % cd(cl(alpha))
 
 % total drag
 % cd_model is profile drag
-const = 1800;
-C_Dtot = C_Di * const + cd_model; % GOOD
+C_Dtot = C_Di + cd_model; % GOOD
 
 
 
 % PLOT coeffs vs alpha
 figure; hold on
 
-plot(alpha_tip_deg, C_Di*const, Color='b');
+plot(alpha_tip_deg, C_Di);
 
-plot(alpha_tip_deg, cd_model, Color='g')
+plot(alpha_tip_deg, cd_model)
 
-plot(alpha_tip_deg, C_Dtot, Color='r')
-
+plot(alpha_tip_deg, C_Dtot)
 
 xlabel('Angle of Attack (deg)')
 ylabel('Drag Coefficient')
 legend('Induced Drag','Profile Drag','Total Drag')
 grid on
-%ylim([0 0.05])
+ylim([0 0.05])
 
-% figure; hold on
-% plot(deg2rad(alpha_tip_deg), C_Di, Color='b');
-% xlabel('Angle of Attack (deg)')
-% ylabel('Drag Coefficient')
-% legend('Induced Drag','Profile Drag','Total Drag')
-% ylim([0 0.05])
-% grid on
+
